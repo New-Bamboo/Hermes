bcff4bc3c4128ccf5a4feedb18c7e1d25dbfdf40
Damon Davison <damon@allolex.net>
2012-10-11 13:25:16 +0100

    Backs up dotfiles from main manifest that are present in the local system

--- install.bash
+++ install.bash
@@ -1,4 +1,26 @@
 #!/usr/bin/env bash
+INSTALL_DIR=`pwd`
+TIMESTAMP=`date +%Y%m%d%H%M%S`
+LOGFILE=$INSTALL_DIR/install-$TIMESTAMP.log
+TEMP_MANIFEST=/tmp/hermes_custom_manifest
+touch $TEMP_MANIFEST
+
+function log () {
+  echo -e $@ >> $LOGFILE
+}
+
+function customise_manifest () {
+
+  CONTENT=`cat $INSTALL_DIR/dotfile_manifest`
+
+  for file in $CONTENT
+    do
+      if [ -e $HOME/$file ]
+        then
+          echo $file >> $TEMP_MANIFEST
+      fi
+    done
+}
 
 function handle_error () {
   if [ "$?" != "0" ]
@@ -17,6 +39,17 @@ function install_dependency () {
   HOMEBREW_OUTPUT=`brew install $@ 2>&1`
   handle_error $1 "Homebrew had a problem\n($HOMEBREW_OUTPUT):"
 }
+
+function backup_dotfiles () {
+  customise_manifest
+  cd $HOME
+  tar zcvf $INSTALL_DIR/dotfile_backup-$TIMESTAMP.tar.gz -I $TEMP_MANIFEST >> $LOGFILE 2>&1
+  handle_error "($?)" "Backup failed, please see the install log for details"
+}
+
+log "Starting Hermes installation"
+
+backup_dotfiles
 
 # Check for dependencies
 check_command_dependency brew
