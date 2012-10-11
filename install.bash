796b775530f3f89953ca80059d8d8e2da1b17ae8
Claudio Ortolina <claudio@jumpzero.com>
2012-10-11 18:07:13 +0100

    Fixes install script.

--- install.bash
+++ install.bash
@@ -9,24 +9,34 @@ function log () {
   echo -e $@ >> $LOGFILE
 }
 
-function customise_manifest () {
-  CONTENT=`cat $INSTALL_DIR/dotfile_manifest`
-  for file in $CONTENT
-    do
-      if [ -e $HOME/.$file ]
-        then
-          echo "$HOME/.$file" >> $TEMP_MANIFEST
-      fi
-    done
+function handle_error () {
+  if [ "$?" != "0" ]; then
+    echo -e "$2 $1"
+    exit 1
+  fi
 }
 
+function customise_manifest () {
+  CONTENT=`cat $INSTALL_DIR/dotfile_manifest`
+  for file in $CONTENT; do
+    if [ -e $HOME/.$file ]
+      then
+        echo "$HOME/.$file" >> $TEMP_MANIFEST
+    fi
+  done
+}
 
-function handle_error () {
-  if [ "$?" != "0" ]
-    then
-      echo -e "$2 $1"
-      exit 1
-  fi
+function link_dotfiles () {
+  CONTENT=`cat $INSTALL_DIR/dotfile_manifest`
+  for file in $CONTENT; do
+    SOURCE_FILE=$HOME/.hermes/hermes/$file
+    TARGET_FILE=$HOME/.$file
+    if [ -e $SOURCE_FILE ]; then
+      echo "Symlinking hermes dotfile $file to $HOME"
+      ln -sf $SOURCE_FILE $TARGET_FILE
+      handle_error "Could not link to $TARGET_FILE" "Symlinking:"
+    fi
+  done
 }
 
 function check_command_dependency () {
@@ -34,10 +44,16 @@ function check_command_dependency () {
   handle_error $1 'There was a problem with:'
 }
 
-function install_dependency () {
+function install_homebrew () {
   log "Checking for the presence of $1"
   HOMEBREW_OUTPUT=`brew install $1 2>&1`
   handle_error $1 "Homebrew had a problem\n($HOMEBREW_OUTPUT):"
+}
+
+function remove_homebrew () {
+  log "Removing homebrew recipe $1"
+  HOMEBREW_OUTPUT=`brew uninstall $1 2>&1`
+  handle_error $1 "Homebrew had a problem while removing\n($HOMEBREW_OUTPUT):"
 }
 
 function backup_dotfiles () {
@@ -47,10 +63,38 @@ function backup_dotfiles () {
   handle_error "($?)" "Backup failed, please see the install log for details"
 }
 
+function homebrew_checkinstall_recipe () {
+  brew list $1
+  if [ $? == 0 ]; then
+    echo "Your $1 installation is fine. Doing nothing."
+  else
+    install_homebrew $1
+  fi
+}
+
+function homebrew_checkinstall_vim () {
+  SKIP=`vim --version | grep '+clipboard'`
+  if [[ "$SKIP" != "" ]]; then
+    echo "Your vim installation is fine. Doing nothing."
+  else
+    brew list macvim
+    if [ $? == 0 ]; then
+      remove_homebrew "macvim"
+    else
+      echo "No vim to remove"
+    fi
+    install_homebrew $1
+  fi
+}
+
 function homebrew_dependencies () {
   while read recipe; do
     echo "Installing recipe $recipe"
-    install_dependency $recipe
+    if [[ $recipe == macvim* ]]; then
+      homebrew_checkinstall_vim $recipe
+    else
+      homebrew_checkinstall_recipe $recipe
+    fi
   done < "$INSTALL_DIR/homebrew_dependencies"
 }
 
@@ -63,3 +107,5 @@ check_command_dependency brew
 check_command_dependency rvm
 
 homebrew_dependencies
+
+link_dotfiles
